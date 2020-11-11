0fd4f863a6f74cf20368773b0785ea3f3cc1fb8d
hyoilll <50327128+hyoilll@users.noreply.github.com>
2020-11-12 00:37:09 +0900

    Broswer Router -> HashRouter

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,29 +1,34 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import Home from "../Routes/Home";
 import Search from "../Routes/Search";
 import TV from "../Routes/TV";
-import Header from "./Header"
-import Detail from "../Routes/Detail"
+import Header from "./Header";
+import Detail from "../Routes/Detail";
 
 class MyRouter extends Component {
-    render() {
-        return (
-            <Router>
-                <>
-                    <Header />
-                    <Switch>
-                        <Route path='/' exact component={Home} />
-                        <Route path='/tv' exact component={TV} />
-                        <Route path='/search' exact component={Search} />
-                        <Route path='/movie/:id' component={Detail} />
-                        <Route path='/show/:id' component={Detail} />
-                        <Redirect from="*" to="/" />
-                    </Switch>
-                </>
-            </Router>
-        )
-    }
-};
+  render() {
+    return (
+      <Router>
+        <>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/tv" exact component={TV} />
+            <Route path="/search" exact component={Search} />
+            <Route path="/movie/:id" component={Detail} />
+            <Route path="/show/:id" component={Detail} />
+            <Redirect from="*" to="/" />
+          </Switch>
+        </>
+      </Router>
+    );
+  }
+}
 
 export default MyRouter;
